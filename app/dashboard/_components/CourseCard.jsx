49824f83917d119dd8691c04147814eaf147d305
Harshpreetkaur98 <harshpreet.149kaur@gmail.com>
2024-09-08 02:10:40 +0100

    Explore Other projects. Packages to install before fixing the error-> 'react-markdown' and 'npm install i react-youtube' REST WHOLE TUTORIAL FINISHED!!

--- app/dashboard/_components/CourseCard.jsx
+++ app/dashboard/_components/CourseCard.jsx
@@ -8,7 +8,7 @@ import { db } from '@/configs/db'
 import { eq } from 'drizzle-orm'
 import Link from 'next/link'
 
-function CourseCard({course,refreshData}) {
+function CourseCard({course,refreshData, displayUser=false}) {
 
     const handleOnDelete=async()=>{
         const resp=await db.delete(CourseList)
@@ -29,15 +29,21 @@ function CourseCard({course,refreshData}) {
       </Link>
       <div className='p-2'>
         <h2 className='font-medium text-lg flex justify-between items-center'>{course?.courseOutput?.course_name} 
-            <DropdownOption handleOnDelete={()=>handleOnDelete()}>
+            {!displayUser&& <DropdownOption handleOnDelete={()=>handleOnDelete()}>
                 <HiMiniEllipsisVertical/>
-            </DropdownOption>
+            </DropdownOption>}
         </h2>
         <p className='text-sm text-gray-400 my-1'>{course?.category}</p>
         <div className='flex items-center justify-between'>
             <h2 className='flex gap-2 items-center p-1 bg-green-50 text-green-700 text-sm rounded-sm'><HiOutlineBookOpen/>{course?.courseOutput?.no_of_chapters} Chapters</h2>
             <h2 className='text-sm bg-green-50 text-green-700 p-1 rounded-sm'>{course?.courseOutput?.level}</h2>
         </div>
+        {displayUser&& <div className='flex gap-2 items-center mt-2'>
+          <Image src={course?.userProfileImage} width={35} height={35} 
+          className='rounded-full'
+          />
+          <h2 className='text-sm'>{course?.userName}</h2>
+        </div>}
       </div>
     </div>
   )
--- app/dashboard/explore/page.jsx
+++ app/dashboard/explore/page.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import {db} from '@/configs/db'
+import {CourseList} from 'react'
+import {CourseCard} from '../_components/CourseCard'
 
 function Explore(){
-    return <div>EXPLORE</div>
+
+    const [courseList, setCourseList] = useState();
+    const[pageIndex, setPageIndex] = useState(0);
+
+    useEffect(()=> {
+        GetAllCourse();
+    }, [pageIndex])
+
+    const GetAllCourse=async()=>{
+        const result = await db.select().from(CourseList)
+        .limit(9)
+        .offset(pageIndex * 9);
+        setCourseList(result);
+        console.log(result);
+    }
+
+    return (
+        <div>
+            <h2 className='font-bold text-3xl'>Explore More Projects</h2>
+            <p>Explore more projects built with AI by other users</p>
+
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-5'>
+                {courseList?.map((course, index) => (
+                    <div>
+                        <CourseCard course={course} displayUser={true} />
+                    </div>
+                ))}
+            </div>
+            <div className='flex justify-between mt-5'>
+                {pageIndex != 0&& <Button onCLick={() => setPageIndex(pageIndex -1)}>Prev Page</Button>}
+                
+                <Button onClick={() => setPageIndex(pageIndex + 1)}>Next Page</Button>
+            </div>
+        </div>    
+    )
 }
 
 export default Explore
